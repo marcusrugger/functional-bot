e13573a70027d096fd4729c5b260c44a173eebc3
Marcus Rugger <marcusrugger@yahoo.com>
2016-03-26 00:22:35 -0400

    Some cleanup for the led matrix

--- src/protocol.meledmatrix.cpp
+++ src/protocol.meledmatrix.cpp
@@ -1,11 +1,17 @@
 #include "protocol.meledmatrix.h"
 
 
-// MeLEDMatrixProtocol::MeLEDMatrixProtocol(DigitalPin *pinScl, DigitalPin *pinSda)
-// :   I2CProtocol(pinScl, pinSda)
-// {
-//     _pinScl->setHigh();
-//     _pinSda->setHigh();
+MeLEDMatrixProtocol::MeLEDMatrixProtocol(DigitalPin *pinScl, DigitalPin *pinSda)
+:   I2CProtocol(pinScl, pinSda)
+{
+    _pinScl->setHigh();
+    _pinSda->setHigh();
 
-//     writeByte(0x40);
-// }
+    start();
+    writeByte(ADDR_AUTO);
+    stop();
+
+    start();
+    writeByte(Cmd_DispCtrl);
+    stop();
+}
